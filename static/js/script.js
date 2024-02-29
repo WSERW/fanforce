12114f6548bdeb59dc9a87d7a2335aa5f803ab27
WSERW <sergeyYH@yandex.ru>
2024-02-29 20:53:58 +0300

    modals

--- static/js/script.js
+++ static/js/script.js
@@ -7,8 +7,8 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 50,
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
     },
     breakpoints: {
         // when window width is >= 320px
@@ -46,3 +46,63 @@ questions.forEach(quest => quest.addEventListener('click', function () {
     // }
 }
 ))
+
+
+// nav burger
+let navToggler = document.querySelector('.nav__toggler')
+let nav = document.querySelector('.nav')
+let isNav = false
+navToggler.addEventListener('click', function () {
+    isNav = !isNav
+    nav.classList.toggle('active')
+    if (isNav) {
+        document.body.style.overflow = 'hidden'
+    }
+    else {
+        document.body.style.overflow = 'visible'
+    }
+})
+
+
+// cookie
+
+cookieBtn = document.querySelector('.cookie__btn')
+cookieWrapper = document.querySelector('.cookie__wrapper')
+cookieBtn.addEventListener('click', function () {
+    cookieWrapper.remove()
+})
+
+
+// modal
+
+modalOpenBtns = document.querySelectorAll('.modal__open')
+modalBtn = document.querySelector('.modal__btn')
+modalWrapper = document.querySelector('.modal__wrapper')
+
+modalOpenBtns.forEach(btn => {
+    btn.addEventListener('click', function () {
+        modalWrapper.classList.add('active')
+    })
+});
+
+modalBtn.addEventListener('click', function () {
+    modalWrapper.classList.remove('active')
+})
+
+// success
+
+successOpenBtns = document.querySelectorAll('.banner__button')
+successBtn = document.querySelector('.success__btn')
+successWrapper = document.querySelector('.success__wrapper')
+
+successOpenBtns.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault()
+        successWrapper.classList.add('active')
+        modalWrapper.classList.remove('active')
+    })
+});
+
+successBtn.addEventListener('click', function () {
+    successWrapper.classList.remove('active')
+})
